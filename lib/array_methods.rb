a590d445ea0baef90f83a73899f404d71d241b1f
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-20 20:31:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,17 +7,17 @@ array.sort
 end
 
 def using_reverse(array)
-
+array.reverse
 end
 
 def using_first(array)
-
+array.first
 end
 
 def using_last(array)
-
+array.last
 end
 
 def using_size(array)
-
+array.size
 end
